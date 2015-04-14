8d26c615616281f7cb39d3fb256d0dc6c1c9ea67
Chris Orebaugh <corebaugh@corebaugh.local>
2015-04-13 22:00:17 -0500

    A little bit of everything.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,25 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.backstretch
 //= require turbolinks
 //= require_tree .
+//= require jquery.bxslider.min.js
+
+$(function() {
+    //ANCHOR SCROLL TO NAME
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get 'backstop' => 'pages#backstop'
+  get 'backstop-mobile' => 'pages#backstop-mobile'
+  get 'backstop-product' => 'pages#backstop-product'
+  get 'backstop-web' => 'pages#backstop-web'
+  get 'home' => 'pages#home'
+  get 'investorbridge' => 'pages#investorbridge'
+  get 'ironwood' => 'pages#ironwood'
+  get 'prototype' => 'pages#prototype'
+  get 'sageify' => 'pages#sageify'
+
 end
